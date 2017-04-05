48ca56b56068e268fdc2d0bad17211b58ce43b3e
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2017-04-05 19:49:27 -0300

    load files moved

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
-require_relative 'loader'
+require_relative 'lib/loader'
 
 run My::API
--- lib/loader.rb
+++ lib/loader.rb
@@ -4,12 +4,12 @@ require 'active_record'
 require 'sqlite3'
 require 'pry'
 
-require_relative 'app'
-
-APPLICATION_PATH = File.expand_path(File.dirname(__FILE__))
+APPLICATION_PATH  ||= File.expand_path(File.dirname(__FILE__) + '/../')
+RACK_ENV          ||= 'development'
 
 ActiveRecord::Base.logger = Logger.new('log/database.log')
 configuration = YAML::load(IO.read('config/database.yml'))
-ActiveRecord::Base.establish_connection(configuration['development'])
+ActiveRecord::Base.establish_connection(configuration[RACK_ENV])
 
+Dir["#{APPLICATION_PATH}/app/controllers/*.rb"].each { |file| require file }
 Dir["#{APPLICATION_PATH}/app/models/*.rb"].each { |file| require file }
